742890b90b33fd8b26a10bc167d0d5901dc6133e
Mark Gao <markgao5059@outlook.com>
2020-09-28 20:26:07 -0400

    First trial commmit

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///demo.db"
+db = SQLAlchemy(app)
+
+class Agenda(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String(200), nullable=False, default="None")
+
+    def __repr__(self):
+        return self.id
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+app.run(debug=True)
